f7e5d8a59937cacd351ca648367c6343f648dd2b
johanrdev <johanrothman1988@gmail.com>
2023-01-28 15:42:32 +0100

    Fixed date issue in seed class

--- FitnessTracker.Infrastructure/Migrations/Configuration.cs
+++ FitnessTracker.Infrastructure/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
+using System.Globalization;
 using System.Linq;
 
 namespace FitnessTracker.Infrastructure.Migrations
@@ -23,15 +24,15 @@ namespace FitnessTracker.Infrastructure.Migrations
 
             var reports = new Collection<Report>
             {
-                new Report { Id = 1, Date = DateTime.Today.AddDays(-8), Weight = 78.8 },
-                new Report { Id = 2, Date = DateTime.Today.AddDays(-7), Weight = 79.1 },
-                new Report { Id = 3, Date = DateTime.Today.AddDays(-6), Weight = 79.5 },
-                new Report { Id = 4, Date = DateTime.Today.AddDays(-5), Weight = 80.2 },
-                new Report { Id = 5, Date = DateTime.Today.AddDays(-4), Weight = 80.0 },
-                new Report { Id = 6, Date = DateTime.Today.AddDays(-3), Weight = 79.6 },
-                new Report { Id = 7, Date = DateTime.Today.AddDays(-2), Weight = 79.9 },
-                new Report { Id = 8, Date = DateTime.Today.AddDays(-1), Weight = 79.2 },
-                new Report { Id = 9, Date = DateTime.Today, Weight = 79.5 },
+                new Report { Id = 1, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-8).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 78.8 },
+                new Report { Id = 2, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-7).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.1 },
+                new Report { Id = 3, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-6).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.5 },
+                new Report { Id = 4, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-5).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 80.2 },
+                new Report { Id = 5, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-4).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 80.0 },
+                new Report { Id = 6, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-3).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.6 },
+                new Report { Id = 7, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-2).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.9 },
+                new Report { Id = 8, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(-1).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.2 },
+                new Report { Id = 9, Date = DateTime.Parse(string.Format("{0} 00:00:00", DateTime.Now.Date.AddDays(0).ToString("MM/dd/yyyy")), CultureInfo.GetCultureInfo("en-US")), Weight = 79.5 },
             };
 
             foreach (Report report in reports)
